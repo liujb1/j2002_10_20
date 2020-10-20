4f8197c87b42f3a18e93a8db0c61e35d6d21baa3
liujb1 <73152383+liujb1@users.noreply.github.com>
2020-10-20 19:20:01 +0800

    Update HelloServlet.java

--- src/main/java/com/qf/web/HelloServlet.java
+++ src/main/java/com/qf/web/HelloServlet.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 @WebServlet("/Servlet")
 public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+      //添加数据
+        //处理数据
+        //响应数据
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
