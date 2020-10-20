20100bebd9830602f8c635aa47adb79f8341f735
Liu <1336220227@qq.com>
2020-10-20 19:15:33 +0800

    第一次提交

---
+++ src/test/java/ServletTest.java
@@ -0,0 +1,15 @@
+
+
+import org.junit.Test;
+
+public class ServletTest {
+    @Test
+    public void testDoPost() {
+        System.out.println("do post正确执行了!!!");
+    }
+
+    @Test
+    public void testDoGet() {
+        System.out.println("do get正确执行了!!!");
+    }
+}
